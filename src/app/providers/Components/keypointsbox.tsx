5b31b23921acf5ea6cf1d6bb88aad936042a7fb1
Jonatan Grön <gupfend@swedcon18.com>
2023-09-25 16:12:47 +0200

    key points finish

--- src/app/providers/Components/keypointsbox.tsx
+++ src/app/providers/Components/keypointsbox.tsx
@@ -25,9 +25,14 @@ const Keypointsbox: React.FC<KeyPointsBoxTamplateProps> = ({
 
 }) => {
   return (
-    <div className=" relative  flex justify-center items-center w-[381px] h-[316px] rounded-[25px] ">
+    
+    <div className="relative group  ">
+        <div className="absolute group-hover:-inset-10 bg-[#45AC60]  rounded-[25px] blur opacity-5  ">
+    
+        </div>
+        <div className=" relative  flex justify-center items-center bg-white w-[381px] h-[316px] rounded-[25px]  hover:shadow-2xl  hover:border-[1px] border-[#45AC60] transition duration-600 cursor-pointer">
           <div className=" w-[310px] h-[259px] top-[100px] left-[35px] gap-[24px]">
-            <div className="w-[64px] h-[64px] ">
+            <div className="w-[64px] h-[64px]">
               <div className="relative flex justify-center items-center">
                 <div className=" absolute w-[64px] h-[64px] top-0 rounded-[100%]  bg-[#45AC60] opacity-10 "></div>
 
@@ -42,12 +47,10 @@ const Keypointsbox: React.FC<KeyPointsBoxTamplateProps> = ({
               </div>
             </div>
 
-            <div className="absolute flex flex-col w-[309.34px] h-[171px] bottom-0 gap-[16px] ">
+            <div className="absolute flex flex-col w-[309.34px] h-[171px] mb-5 bottom-0 gap-[16px] ">
               <div className="w-[309.34px] h-[29px] ">
                 <h1 className=" text-[Poppins] text-[24px] font-semibold leading-7 tracking-tight left-0">
                 {header}
-
-
 
                 </h1>
               </div>
@@ -60,7 +63,7 @@ const Keypointsbox: React.FC<KeyPointsBoxTamplateProps> = ({
 
                 </p>
               </div>
-              <div className=" w-[309.34px] h-[22px]">
+              <div className=" w-[309.34px] h-[22px] ">
             
 
               <a href={linkto} className=" absolute bottom-0 text-[#000000] underline hover:text-blue-700 cursor-pointer">{linktext}</a>
@@ -68,6 +71,7 @@ const Keypointsbox: React.FC<KeyPointsBoxTamplateProps> = ({
 
             </div>
           </div>
+        </div>
         </div>
   );
 };
