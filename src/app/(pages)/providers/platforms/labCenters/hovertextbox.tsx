5dcf05f24edbed45b078df6ea1fc94c12fd0d7ee
jonatangroncode <jonatan.gron.code@hotmail.com>
2024-01-11 14:44:10 +0100

    added hover effekts labcenter-joincarleyenables-hovertextbox

--- src/app/(pages)/providers/platforms/labCenters/hovertextbox.tsx
+++ src/app/(pages)/providers/platforms/labCenters/hovertextbox.tsx
@@ -14,13 +14,22 @@ const Hovertextbox: React.FC<HovertextboxTamplateProps> = ({
   // ... andra props för varje sektion av din mall
 }) => {
   return (
-    <div className=" w-[594.24px] h-[142px] gap-[16px]">
-      <div className=" w-[594.24px] h-[126px] gap-[16px] ">
-        <h1 className="w-[442px] h-[32px] text-[poppins] text-[24px] font-semibold leading-8 text-left ">
-            {header01}</h1>
+    <div className="relative group">
+      <div className="absolute group-hover:-inset-10 bg-[#45AC60] rounded-[25px] blur opacity-5  "></div>
 
-        <p className="text-[poppins] text-[16px] font-normal leading-6 tracking-tight text-left opacity-50">
-            {paragraph01}</p>
+      <div className=" relative flex flex-col justify-center items-center w-[629px] h-[180px] bg-white  rounded-[25px]  hover:border-[1px] border-[#45AC60] ">
+        <div className="  w-[594.24px] h-[122px] gap-[16px] rounded-[25px] ">
+          <div className=" w-[594.24px] h-[126px] gap-[16px] ">
+            <h1 className="w-[442px] h-[32px] text-[poppins] text-[24px] font-semibold leading-8 text-left ">
+              {header01}
+            </h1>
+
+            <p className="text-[poppins] text-[16px] font-normal leading-6 tracking-tight text-left opacity-50">
+              {paragraph01}
+            </p>
+          </div>
+          <div className="w-[526px] border-[1px] left-0  border-[#45AC60] group-hover:hidden "></div>
+        </div>
       </div>
     </div>
   )
