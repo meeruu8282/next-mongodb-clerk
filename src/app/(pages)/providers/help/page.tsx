024663018c2c69007832147d7bd94a28a6794cd5
Johnjr1 <114564748+Johnjr1@users.noreply.github.com>
2024-01-16 10:36:22 +0100

    worked on searchbar

--- src/app/(pages)/providers/help/page.tsx
+++ src/app/(pages)/providers/help/page.tsx
@@ -1,11 +1,12 @@
 import BlurBackGroundDesign from "../Components/blurBackGroundDesign";
+import SearchBar from "../Components/searchBar";
 import SuperCharge from "../Components/superCharge";
 
 export default function Help() {
   return (
-    <div className="flex justify-center ">
+    <div className="justify-center items-center min-h-screen">
       <BlurBackGroundDesign/>
-      
+      <SearchBar/>
        {/* Code in components folder then superCarge */}
      <SuperCharge/>
     </div>
