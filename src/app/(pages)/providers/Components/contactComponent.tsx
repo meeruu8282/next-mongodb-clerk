0f55c18e1dbd7009414fefa3032e150b71b737a3
Johnjr1 <114564748+Johnjr1@users.noreply.github.com>
2024-01-19 12:19:04 +0100

    created button and paragraf

--- src/app/(pages)/providers/Components/contactComponent.tsx
+++ src/app/(pages)/providers/Components/contactComponent.tsx
@@ -1,7 +1,10 @@
 export default function ContactComponent(){
     return(
-        <div>
-            <button>contact us</button>
+        <div className="flex flex-col justify-start w-[1258px] mt-[100px]">
+            <div>
+                <button className="carelyoButton bg-opacity-10 text-[#45AC60] text-[poppins] font-semibold hover:text-[#FFFFFF]">Contact Us</button>
+                <p className="text-[Poppins] text-[16px] leading-[22px] tracking-[0em] text-[#9C9C9C] mt-6">Send us your questions, comments or suggestions and we will reply you as quickly as possible. Please read through our <a className="text-[#45AC60] text-opacity-50 underline" href="">FAQ</a> there's a good chance an answer to your question is there.</p>
+            </div>
         </div>
     )
 }
