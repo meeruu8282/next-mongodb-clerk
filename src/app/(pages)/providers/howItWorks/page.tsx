467bff0880faafd0a3cac8ee628e6f5109efc96a
Anton Sjöstrand <asjoa99@hotmail.com>
2024-01-24 14:02:26 +0100

    cleaning up some code in page file

--- src/app/(pages)/providers/howItWorks/page.tsx
+++ src/app/(pages)/providers/howItWorks/page.tsx
@@ -63,7 +63,7 @@ export default function Howitwork() {
       </div>
 
       <div className="flex border border-blue-500 ">
-        <div className="leftside-of-howitwork w-full  flex flex-col items-center mt-[100px] border border-yellow-400   relative">
+        <div className="leftside-of-howitwork w-full  flex flex-col items-center mt-[100px] ">
           <div className="bg-red-300 flex items-center justify-center w-[185px] h-[66px] mb-[20px] pr-1 border ">
             <div className="w-[66px] h-[66px]">
               <Image
@@ -82,28 +82,16 @@ export default function Howitwork() {
        
 
        
-       
+          <div className="w-full flex justify-center items-center mt-10">
           <HowItWorksPartOne 
            hashtagNumber={"#1"} 
            header={" Registration & Profile Creation"} 
            imageSrc={"/iPadPro.png"}
            text1={"Registering as healthcare service providers."} 
            text2={"Create a comprehensive profile by entering their personal information"}/>
-          
-
-          
-
-          <div className="absolute right-[60px] top-[732px] transform -translate-y-[50%]">
-            <Image
-              className="  "
-              layout="fixed"
-              width={42}
-              height={204}
-              src="/Vector677.svg"
-              alt="677"
-              priority
-            />
           </div>
+
+          
 
           <div className="w-full flex justify-center items-center mt-10">
 
@@ -128,7 +116,7 @@ export default function Howitwork() {
           />
         </div>
 
-        <div className="right side  w-full  bg-blue-500 ">
+        <div className="right side w-full ">
           <div className=" flex justify-center   ">
             <div className="flex justify-center">
               <div className=" flex  items-center justify-between w-[185px] h-[66px] mt-[100px]  border  ">
@@ -154,18 +142,6 @@ export default function Howitwork() {
 
 
 
-      <div className=" w-full flex justify-center items-center">
-        <div className="w-[100%] ">
-          <HowItWorksPartFive
-            hashtagNumber={"#5"}
-            header={"Consultation and Care"}
-            imageSrc1={"/ipadimagehowItWorks708.png"}
-            imageSrc2={"/mobilePhonehowitworksimage707.png"}
-            text1={"During the consultation, providers engage with the client, conduct assessments, diagnose conditions, recommend treatments, and provide medical advice."}
-            text2={"Virtual consultation or visit your provider in person. Discuss your health concerns, receive medical advice, and ask any questions you may have."}
-          />
-        </div>
-        </div>
 
       <div className="flex">
       <HowItWorksPartFour 
@@ -177,24 +153,6 @@ export default function Howitwork() {
       </div>
 
 
-
-
-
-
-
-      <div className="w-full flex justify-center items-center mt-8">
-        <HowItWorksBenefits
-          image={"/Rectangle34624354.png"}
-          header={"THE BENEFITS OF JOINING CARELYO AS A PROVIDER"}
-          checkmarkboxheader1={"Expanded Reach and Patient Base"}
-          checkmarkboxparagraph1={"Reach a larger pool of potential patients, including those who prefer the convenience of online consultations. "}
-          checkmarkboxheader2={"Easy Appointment Management"}
-          checkmarkboxparagraph2={"Through the integrated scheduling system, you can effortlessly manage and organize appointments, set availability, and view upcoming consultations. "}
-          checkmarkboxheader3={"Synchronized Patient Communication"}
-          checkmarkboxparagraph3={"Communication between providers and patients using the platform's you can interact with patients before, during, and after consultations."}
-        />
-        </div>
-          
       <div className=" w-full flex justify-center items-center">
         <div className="w-[100%] ">
           <HowItWorksPartFive
@@ -206,15 +164,9 @@ export default function Howitwork() {
             text2={"Virtual consultation or visit your provider in person. Discuss your health concerns, receive medical advice, and ask any questions you may have."}
           />
         </div>
-      </div>
+        </div>
 
-
-
-
-
-
-
-
+    
       <div className="w-full flex justify-center items-center mt-8">
         <HowItWorksBenefits
           image={"/Rectangle34624354.png"}
