c1641bab6439dc6c1596624efa910f811179d20a
Johnjr1 <114564748+Johnjr1@users.noreply.github.com>
2024-01-24 11:11:05 +0100

    fixed blurbackground placement

--- src/app/(pages)/providers/help/page.tsx
+++ src/app/(pages)/providers/help/page.tsx
@@ -7,7 +7,7 @@ import SuperCharge from "../Components/superCharge";
 
 export default function Help() {
   return (
-    <div className="flex flex-col justify-center align-center items-center min-h-screen">
+    <div className="flex flex-col align-center items-center min-h-screen">
       <BlurBackGroundDesign/>
       <SearchBar/>
       <HelpCenter/>
