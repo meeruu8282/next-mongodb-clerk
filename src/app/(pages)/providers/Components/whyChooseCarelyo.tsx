5762e3cbcbf168664fdae32a239b875bc902d0a0
Johnjr1 <114564748+Johnjr1@users.noreply.github.com>
2024-02-29 14:42:38 +0100

    improved responsivity (almost done)

--- src/app/(pages)/providers/Components/whyChooseCarelyo.tsx
+++ src/app/(pages)/providers/Components/whyChooseCarelyo.tsx
@@ -31,7 +31,7 @@ const WhychooseCarelyo: React.FC<WhychooseCarelyoTamplateProps> = ({
     const flexDirection = reverseDirection ? "row-reverse" : "row";
   return (
     <div className= "flex justify-center items-center h-[full] top-[1870px] pb-[60px] pt-[40px] mt-20">
-    <div className={`flex flex-${flexDirection} justify-center w-[90%] items-center md:w-[1192px] h-[full] gap-[114px] `}>
+    <div className={`flex flex-${flexDirection} justify-center items-center md:w-[1192px] h-[full] gap-[114px] `}>
       <div className="w-[449px] h-[449px] hidden md:block">
         <Image
           className="rounded-[25px]"
@@ -42,20 +42,20 @@ const WhychooseCarelyo: React.FC<WhychooseCarelyoTamplateProps> = ({
           alt="costomimage"
         />
       </div>
-      <div className="w-[full] h-[full] gap-[32px] flex flex-col">
-        <div className=" relative h-[304px] w-[629px] gap-[24px] ">
-          <div className=" w-[514px] h-[120px]">
+      <div className="w-[full] gap-[32px] flex flex-col">
+        <div className=" relative w-[full] gap-[24px] flex flex-col items-start px-[35px] md:px-[0px]">
+          <div className="md:w-[514px]">
             <h1 className="text-[poppins] text-[40px] font-normal leading-60 tracking-tighter text-left">
               {header}
             </h1>
           </div>
-          <div className="w-[4/6] md:w-[629px] h-[190px] opacity-[50%] bottom-0  ">
+          <div className="w-[4/6] md:w-[629px] opacity-[50%] bottom-0">
             <p className="text-[Poppins] text-[20px] font-normal leading-8 tracking-tighter text-left">
               {paragraph}
             </p>
           </div>
         </div>
-        <div className="w-[full] h-[full] md:hidden">
+        <div className="w-[full] md:hidden flex justify-center px-[25px]">
         <Image
           className="rounded-[25px]"
           layout="fixed "
@@ -65,7 +65,7 @@ const WhychooseCarelyo: React.FC<WhychooseCarelyoTamplateProps> = ({
           alt="customimage"
         />
       </div>
-        <div className="flex flex-col h-[72px]" >
+        <div className="flex flex-col h-[72px] items-center md:items-start">
         <Link href="/providers/pricing" passHref>
            <CarelyoButton
           buttonText={buttonsText}
@@ -73,9 +73,6 @@ const WhychooseCarelyo: React.FC<WhychooseCarelyoTamplateProps> = ({
         />  
         </Link>
         </div>
-
-
-       
       </div>
     </div>
   </div>
