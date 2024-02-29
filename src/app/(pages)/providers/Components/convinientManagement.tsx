72dfb9442491733ab8374af8dd5aae55489984af
PhilipSamuelsson <philip.samuelsson@iths.se>
2024-02-29 11:23:10 +0100

    Refactor ConvinientManagement component

--- src/app/(pages)/providers/Components/convinientManagement.tsx
+++ src/app/(pages)/providers/Components/convinientManagement.tsx
@@ -1,20 +1,15 @@
-import React from "react";
-import Image from "next/image";
+import React from 'react'
+import Image from 'next/image'
 
 interface ConvinientManagementTemplateProps {
-  text: string;   // string
+  text: string // string
 
   // ... other props for sections of the tamplate
 }
 
-const ConvinientManagement: React.FC<ConvinientManagementTemplateProps> = ({
-
-  text,
-
-
-}) => {
+const ConvinientManagement: React.FC<ConvinientManagementTemplateProps> = ({ text }) => {
   return (
-    <div className=" absolute flex flex-row items-center justify-center w-[294px] h-[74px] top-[370px]  p-[25px 22px] rounded-[20px] gap-[8px] bg-white shadow-[5%]">
+    <div className="static lg:absolute flex flex-row items-center justify-center w-[294px] h-[74px] top-[370px]  p-[25px 22px] rounded-[20px] gap-[8px] bg-white shadow-[5%] mx-auto">
       <Image
         className=""
         layout="fixed "
@@ -26,14 +21,11 @@ const ConvinientManagement: React.FC<ConvinientManagementTemplateProps> = ({
 
       <div className="w-[218px] h-[24px] flex justify-center items-center">
         <h1 className=" text-[Poppins] text-[16px] font-normal leading-[24px] tracking-tighter text-left ">
-
-
-     {text}
-
+          {text}
         </h1>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ConvinientManagement;
+export default ConvinientManagement
