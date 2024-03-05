f028fd76b6ad9c8d08bc2ff236d9d07f3c5fdcb6
ShahadAlkazzaz <shahad.alqazzaz@yahoo.com>
2024-03-05 12:22:19 +0100

    fixed div

--- src/app/(pages)/providers/Components/NavBar/NavbarMobile.tsx
+++ src/app/(pages)/providers/Components/NavBar/NavbarMobile.tsx
@@ -52,33 +52,31 @@ const NavbarMobile = () => {
         }`}
       >
         <div className="nav-links flex flex-col items-center justify-center h-full w-full text-4xl gap-9 pb-20">
-          {/* "Black navbar in mobile screen prototype"
-
-          Still needs to add functionality to the select tag
-          */}
-          <div className="mx-auto p-5">
-            <select
-              name="section"
-              id="section"
-              className=" text-base font-semibold border-4 border-solid border-sage bg-white w-full h-12 rounded-[35px] text-center"
-            >
-              <option value="patients">FOR PATIENTS</option>
-              <option value="providers">FOR PROVIDERS</option>
-              <option value="partner">FOR PARTNERS</option>
-              <option value="shop">FOR SHOP</option>
-            </select>
-          </div>
-          {/* "End of black navbar in mobile screen prototype" */}
           <Link
             onClick={closeMenu}
             href="/providers/howItWorks"
-            className="text-black hover:text-sage"
+            className="text-black hover:text-sage relative group border-b-2 border-sage"
           >
             <span>How it works</span>
           </Link>
-          <div>
-            <div className="text-black text-center hover:text-sage" onClick={handlePlatformHover}>
+          <div className="relative group">
+            <div
+              className="text-black text-center hover:text-sage border-b-2 border-sage"
+              onClick={handlePlatformHover}
+            >
               Platform
+            </div>
+            <div className="absolute top-1/2 right-[-16%] transform -translate-y-1/2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={3}
+                className={`w-5 h-5 text-sage ${isPlatformOpen ? 'hidden' : ''}`}
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
+              </svg>
             </div>
             <ul
               className={`${
@@ -141,17 +139,26 @@ const NavbarMobile = () => {
               </li>
             </ul>
           </div>
+
           <Link
             onClick={closeMenu}
             href="/providers/pricing"
-            className="text-black hover:text-sage"
+            className="text-black hover:text-sage relative group border-b-2 border-sage"
           >
             <span>Pricing</span>
           </Link>
-          <Link onClick={closeMenu} href="/providers/blog" className="text-black hover:text-sage relative group border-b-2 border-sage">
+          <Link
+            onClick={closeMenu}
+            href="/providers/blog"
+            className="text-black hover:text-sage relative group border-b-2 border-sage"
+          >
             <span>Blog</span>
           </Link>
-          <Link onClick={closeMenu} href="/providers/help" className="text-black hover:text-sage relative group border-b-2 border-sage ">
+          <Link
+            onClick={closeMenu}
+            href="/providers/help"
+            className="text-black hover:text-sage relative group border-b-2 border-sage "
+          >
             <span>Help</span>
           </Link>
           <div className="flex w-[125px] h-[52px] border-[1px] border-sage rounded-[35px] justify-center items-center gap-2">
@@ -169,9 +176,11 @@ const NavbarMobile = () => {
               <i className="fa fa-chevron-down text-[10px]"></i>
             </div>
           </div>
-          <div className="">
-            <button className="carelyoButton lg:block">
-              <div className="carelyoButtonText flex items-center justify-center text-[16px] w-full">
+
+        <div className="">
+          <Link href="/providers/pricing">
+            <div className="carelyoButton lg:block" onClick={closeMenu}>
+              <div className="carelyoButtonText flex items-center justify-center text-[16px] w-full h-full">
                 Get Carelyo
                 <div className="ml-3">
                   <div className="h-[28px] w-[28px] rounded-[100px] flex justify-center items-center bg-white bg-opacity-[25%]">
@@ -192,8 +201,9 @@ const NavbarMobile = () => {
                   </div>
                 </div>
               </div>
-            </button>
-          </div>
+            </div>
+          </Link>
+        </div>
         </div>
       </div>
     </>
