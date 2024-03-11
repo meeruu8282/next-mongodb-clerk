22fca196b84c1c7da13f2c2c3d62680c82f0bbb2
Anton Sjöstrand <asjoa99@hotmail.com>
2024-03-11 16:51:24 +0100

    add props and call on them in page file

--- src/app/(pages)/partners/howitworks/howItWorksSection.tsx
+++ src/app/(pages)/partners/howitworks/howItWorksSection.tsx
@@ -1,39 +1,96 @@
 import React from 'react'
 import Image from "next/image";
 
-function HowItWorksSection() {
+
+
+
+
+
+interface HowItWorksSectionTemplateProps {
+    heading: string;
+    number1: string;
+  number2: string;
+  number3: string;
+  number4: string;
+  text1: string;
+  text2: string;
+  text3: string;
+  text4: string;
+  header1: string;
+  header2: string;
+  header3: string;
+  header4: string;
+  image1: string;
+  image2: string;
+  image3: string;
+  image4: string;
+
+
+
+  
+  
+   
+  
+    // ... andra props för varje sektion av din mall
+  }
+  
+  const HowItWorksSection: React.FC<HowItWorksSectionTemplateProps> = ({
+    heading,
+    number1,
+number2,
+number3,
+number4,
+text1,
+text2,
+text3,
+text4,
+header1,
+header2,
+header3,
+header4,
+image1,
+image2,
+image3,
+image4,
+ 
+   
+    
+  
+    // ... andra props för varje sektion av din mall
+  }) => {
   return (
     <div className='w-full flex justify-center items-center '>
-        <div className='w-[90%] border border-black'>
+        <div className='w-[90%] '>
             <div className='flex justify-start items-center mb-4'>
-                <h1 className='font-bold text-[Poppins] text-2xl sm:text-2xl md:text-2xl lg:text-4xl'>How It Works</h1>
+                <h1 className='font-bold text-[Poppins] text-2xl sm:text-2xl md:text-2xl lg:text-4xl'>{heading}</h1>
             </div>
 
             <div className='flex flex-col gap-8'>
                 
-            <div className='flex sm:flex-row flex-col max-h-[33.3rem] gap-3 justify-center items-center '>
+            <div className='flex sm:flex-row flex-col max-h-[33.3rem] gap-3 justify-center items-center border border-black'>
                 <div className='leftside w-[80%] sm:w-1/2 flex flex-col justify-center items-start h-auto  gap-2'>
                     <div className='flex flex-row gap-2'>
                         <div className='bg-greenText bg-opacity-50 rounded-full flex justify-center items-center w-[1.7rem] h-[1.7rem] sm:w-[2rem] sm:h-[2rem] md:w-[2.2rem] md:h-[2.2rem] lg:w-[2.5rem] lg:h-[2.5rem]'>
-                            <h2 className='text-greenText text-[Poppins] font-normal text-base sm:text-base md:text-lg lg:text-lg xl:text-lg'>1</h2>
+                            <h2 className='text-greenText text-[Poppins] font-normal text-base sm:text-base md:text-lg lg:text-lg xl:text-lg'>{number1}</h2>
                         </div>
                         <div className='flex justify-center items-center'>
-                            <h1 className='font-bold text-[Poppins] text-xl sm:text-2xl md:text-2xl lg:text-4xl'>Register on our Website</h1>
+                            <h1 className='font-bold text-[Poppins] text-xl sm:text-2xl md:text-2xl lg:text-4xl'>{header1}</h1>
                         </div>
 
                     </div>
 
                     <div className='flex justify-center items-center sm:pl-11 pl-9'>
-                        <p className='text-[Poppins] font-normal text-base sm:text-base md:text-base lg:text-base xl:text-lg opacity-50'>Begin the partnership process by visiting our website and accessing the partnership registration page. Fill out and submit the partnership registration form with your organization's details.</p>
+                        <p className='text-[Poppins] font-normal text-base sm:text-base md:text-base lg:text-base xl:text-lg opacity-50'>{text1}</p>
                     </div>
 
                 </div>
                 <div className='rightside w-1/2 h-auto  hidden sm:block'>
                 <Image
+         className='max-h-[33.1rem]'
            layout="responsive"
                width={510}
                height={424}
-               src="/LaptopHowItsWork.png"
+               src={image1}
                alt="mobilephone"
              />
 
@@ -46,26 +103,27 @@ function HowItWorksSection() {
                 <div className='leftside w-[80%] sm:w-1/2 flex flex-col justify-center items-start h-auto  gap-2'>
                     <div className='flex flex-row gap-2'>
                         <div className='bg-greenText bg-opacity-50 rounded-full flex justify-center items-center w-[1.7rem] h-[1.7rem] sm:w-[2rem] sm:h-[2rem] md:w-[2.2rem] md:h-[2.2rem] lg:w-[2.5rem] lg:h-[2.5rem]'>
-                            <h2 className='text-greenText text-[Poppins] font-normal text-base sm:text-base md:text-lg lg:text-lg xl:text-lg'>1</h2>
+                            <h2 className='text-greenText text-[Poppins] font-normal text-base sm:text-base md:text-lg lg:text-lg xl:text-lg'>{number2}</h2>
                         </div>
                         <div className='flex justify-center items-center'>
-                            <h1 className='font-bold text-[Poppins] text-xl sm:text-2xl md:text-2xl lg:text-4xl'>Register on our Website</h1>
+                            <h1 className='font-bold text-[Poppins] text-xl sm:text-2xl md:text-2xl lg:text-4xl'>{header2}</h1>
                         </div>
 
                     </div>
 
                     <div className='flex justify-center items-center sm:pl-11 pl-9'>
-                        <p className='text-[Poppins] font-normal text-base sm:text-base md:text-base lg:text-base xl:text-lg opacity-50'>Begin the partnership process by visiting our website and accessing the partnership registration page. Fill out and submit the partnership registration form with your organization's details.</p>
+                        <p className='text-[Poppins] font-normal text-base sm:text-base md:text-base lg:text-base xl:text-lg opacity-50'>{text2}</p>
                     </div>
 
                 </div>
-                <div className='rightside w-1/2 h-auto  hidden sm:block'>
+                <div className='rightside w-1/2 h-auto hidden sm:block'>
                 <Image
+                  className='max-h-[33.1rem]'
            layout="responsive"
-               width={510}
+             width={510}
                height={424}
-               src="/LaptopHowItsWork.png"
-               alt="mobilephone"
+               src={image2}
+               alt="number2"
              />
 
                 </div>
@@ -75,25 +133,26 @@ function HowItWorksSection() {
                 <div className='leftside w-[80%] sm:w-1/2 flex flex-col justify-center items-start h-auto  gap-2'>
                     <div className='flex flex-row gap-2'>
                         <div className='bg-greenText bg-opacity-50 rounded-full flex justify-center items-center w-[1.7rem] h-[1.7rem] sm:w-[2rem] sm:h-[2rem] md:w-[2.2rem] md:h-[2.2rem] lg:w-[2.5rem] lg:h-[2.5rem]'>
-                            <h2 className='text-greenText text-[Poppins] font-normal text-base sm:text-base md:text-lg lg:text-lg xl:text-lg'>1</h2>
+                            <h2 className='text-greenText text-[Poppins] font-normal text-base sm:text-base md:text-lg lg:text-lg xl:text-lg'>{number3}</h2>
                         </div>
                         <div className='flex justify-center items-center'>
-                            <h1 className='font-bold text-[Poppins] text-xl sm:text-2xl md:text-2xl lg:text-4xl'>Register on our Website</h1>
+                            <h1 className='font-bold text-[Poppins] text-xl sm:text-2xl md:text-2xl lg:text-4xl'>{header3}</h1>
                         </div>
 
                     </div>
 
                     <div className='flex justify-center items-center sm:pl-11 pl-9'>
-                        <p className='text-[Poppins] font-normal text-base sm:text-base md:text-base lg:text-base xl:text-lg opacity-50'>Begin the partnership process by visiting our website and accessing the partnership registration page. Fill out and submit the partnership registration form with your organization's details.</p>
+                        <p className='text-[Poppins] font-normal text-base sm:text-base md:text-base lg:text-base xl:text-lg opacity-50'>{text3}</p>
                     </div>
 
                 </div>
                 <div className='rightside w-1/2 h-auto  hidden sm:block'>
                 <Image
+                  className='max-h-[33.1rem]'
            layout="responsive"
                width={510}
                height={424}
-               src="/LaptopHowItsWork.png"
+               src={image3}
                alt="mobilephone"
              />
 
@@ -104,25 +163,26 @@ function HowItWorksSection() {
                 <div className='leftside w-[80%] sm:w-1/2 flex flex-col justify-center items-start h-auto  gap-2'>
                     <div className='flex flex-row gap-2'>
                         <div className='bg-greenText bg-opacity-50 rounded-full flex justify-center items-center w-[1.7rem] h-[1.7rem] sm:w-[2rem] sm:h-[2rem] md:w-[2.2rem] md:h-[2.2rem] lg:w-[2.5rem] lg:h-[2.5rem]'>
-                            <h2 className='text-greenText text-[Poppins] font-normal text-base sm:text-base md:text-lg lg:text-lg xl:text-lg'>1</h2>
+                            <h2 className='text-greenText text-[Poppins] font-normal text-base sm:text-base md:text-lg lg:text-lg xl:text-lg'>{number4}</h2>
                         </div>
                         <div className='flex justify-center items-center'>
-                            <h1 className='font-bold text-[Poppins] text-xl sm:text-2xl md:text-2xl lg:text-4xl'>Register on our Website</h1>
+                            <h1 className='font-bold text-[Poppins] text-xl sm:text-2xl md:text-2xl lg:text-4xl'>{header4}</h1>
                         </div>
 
                     </div>
 
                     <div className='flex justify-center items-center sm:pl-11 pl-9'>
-                        <p className='text-[Poppins] font-normal text-base sm:text-base md:text-base lg:text-base xl:text-lg opacity-50'>Begin the partnership process by visiting our website and accessing the partnership registration page. Fill out and submit the partnership registration form with your organization's details.</p>
+                        <p className='text-[Poppins] font-normal text-base sm:text-base md:text-base lg:text-base xl:text-lg opacity-50'>{text4}</p>
                     </div>
 
                 </div>
                 <div className='rightside w-1/2 h-auto  hidden sm:block'>
                 <Image
+                  className='max-h-[33.1rem]'
            layout="responsive"
                width={510}
                height={424}
-               src="/LaptopHowItsWork.png"
+               src={image4}
                alt="mobilephone"
              />
 
--- src/app/(pages)/partners/howitworks/page.tsx
+++ src/app/(pages)/partners/howitworks/page.tsx
@@ -11,36 +11,55 @@ function HowItsWorks() {
   return (
     <div className='w-screen flex flex-col justify-center items-center'>
       <div className='w-full flex justify-center items-center sm:mb-14 mb-5'>
-      
+
         <FirstPartnerplatform
-            headerBlackText1={"97% of partners say using"}
-            headerGreenText1={"our platform"}
-            headerBlackText2="is satisfying."
-            paragraph1={
-              "Welcome to our partnership program! We value collaboration and aim to create mutually beneficial relationships in the healthcare industry"
-            }
-          />
+          headerBlackText1={"97% of partners say using"}
+          headerGreenText1={"our platform"}
+          headerBlackText2="is satisfying."
+          paragraph1={
+            "Welcome to our partnership program! We value collaboration and aim to create mutually beneficial relationships in the healthcare industry"
+          }
+        />
       </div>
-      <div className='w-full flex justify-center items-center'>
-      <Partnership number1={'482'}
-       number2={'2000+'}
-        number3={'232'}
-        number4={'21'} 
-        text1={'Family Physician'}
-        text2={'Independent Doctor'}
-        text3={'Clinicor Hospital'}
-        text4={'Country used carelyo'}/>
+      <div className='w-full flex justify-center items-center mb-14'>
+        <Partnership number1={'482'}
+          number2={'2000+'}
+          number3={'232'}
+          number4={'21'}
+          text1={'Family Physician'}
+          text2={'Independent Doctor'}
+          text3={'Clinicor Hospital'}
+          text4={'Country used carelyo'} />
 
       </div>
 
-    
-      <HowItWorksSection/>
 
-  
+      <HowItWorksSection heading={''}
+        number1={'1'}
+        text1={'Begin the partnership process by visiting our website and accessing the partnership registration page. Fill out and submit the partnership registration form with your organizations details.'}
+        header1={'Register on our Website'}
+        image1={'/LaptopHowItWorks.png'}
 
-      
+        number2={'2'}
+        text2={'Our partnership team will carefully review your registration submission and evaluate your organization based on criteria such as qualifications, expertise, resources, and potential synergy with our platform.'}
+        header2={'Evaluation and Assessment'}
+        image2={'/TwoPersonTalkHowItWorks.png'}
 
-    
+        number3={'3'}
+        text3={'Our collaboration plan aims to leverage each partner strengths and resources to achieve our shared objectives'}
+        header3={'Collaboration Planning'}
+        image3={'/GroupTalkHowItWorks.png'}
+
+        number4={'4'}
+        text4={'With the collaboration plan in place, we will initiate the implementation phase, working together to execute the agreed-upon activities.'}
+        header4={'Implementation and Execution'}
+        image4={'/DoctorLaptopHowItWorks.png'} />
+
+
+
+
+
+
 
 
 
