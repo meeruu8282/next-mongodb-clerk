d271e59f61cd9f3c97730a05d1257ea96f428a5f
Anton Sjöstrand <asjoa99@hotmail.com>
2024-03-12 16:12:54 +0100

    clean up the code and made the circle better

--- src/app/(pages)/partners/Components/partnership.tsx
+++ src/app/(pages)/partners/Components/partnership.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
-
-
 interface PartnershipTemplateProps {
   number1: string;
   number2: string;
@@ -11,69 +9,55 @@ interface PartnershipTemplateProps {
   text2: string;
   text3: string;
   text4: string;
-
-
- 
-
-  // ... andra props för varje sektion av din mall
 }
 
 const Partnership: React.FC<PartnershipTemplateProps> = ({
-number1,
-number2,
-number3,
-number4,
-text1,
-text2,
-text3,
-text4
- 
-  
+  number1,
+  number2,
+  number3,
+  number4,
+  text1,
+  text2,
+  text3,
+  text4
 
-  // ... andra props för varje sektion av din mall
 }) => {
   return (
     <div className='flex flex-col justify-center items-center w-full'>
-    
-          <div className='flex flex-col sm:flex-row justify-between items-center gap-3 w-[95%] lg:w-[95%] xl:w-[80%]'>
-              <div className='flex flex-col justify-center items-center max-w-[13rem] w-full gap-3'>
-              <div className='flex justify-center items-center'>
-                <h2 className='font-bold text-[Poppins] text-2xl sm:text-3xl md:text-3xl lg:text-4xl'>{number1}</h2>
-                </div>
-                <div>
-                <p className='text-[Poppins] font-normal text-base sm:text-base md:text-base lg:text-xl xl:text-xl opacity-50'>{text1}</p>
-                </div>
-              </div>
-              <div className='flex flex-col justify-center items-center max-w-[13rem] w-full gap-3'>
-              <div className='flex justify-center items-center'>
-                <h2 className='font-bold text-[Poppins] text-2xl sm:text-3xl md:text-3xl lg:text-4xl'>{number2}</h2>
-                </div>
-                <div>
-                <p className='text-[Poppins] font-normal text-base sm:text-base md:text-base lg:text-xl xl:text-xl   opacity-50'>{text2}</p>
-                </div>
-              </div>
-              <div className='flex flex-col justify-center items-center max-w-[13rem] w-full gap-3'>
-              <div className='flex justify-center items-center'>
-                <h2 className='font-bold text-[Poppins] text-2xl sm:text-3xl md:text-3xl lg:text-4xl'>{number3}</h2>
-                </div>
-                <div>
-                <p className='text-[Poppins] font-normal text-base sm:text-base md:text-base lg:text-xl xl:text-xl   opacity-50'>{text3}</p>
-                </div>
-              </div>
-              <div className='flex flex-col justify-center items-center max-w-[13rem] w-full gap-3'>
-              <div className='flex justify-center items-center'>
-                <h2 className='font-bold text-[Poppins] text-2xl sm:text-3xl md:text-3xl lg:text-4xl'>{number4}</h2>
-                </div>
-                <div>
-                <p className='text-[Poppins] font-normal text-base sm:text-base md:text-base lg:text-xl xl:text-xl  opacity-50'>{text4}</p>
-                </div>
-              </div>
-            
-          
-           
+      <div className='flex flex-col sm:flex-row justify-between items-center gap-3 w-[95%] lg:w-[95%] xl:w-[80%]'>
+        <div className='flex flex-col justify-center items-center max-w-[13rem] w-full gap-3'>
+          <div className='flex justify-center items-center'>
+            <h2 className='font-bold text-[Poppins] text-2xl sm:text-3xl md:text-3xl lg:text-4xl text-greenText'>{number1}</h2>
           </div>
-
-        
+          <div>
+            <p className='text-[Poppins] font-normal text-base sm:text-base md:text-base lg:text-xl xl:text-xl opacity-50'>{text1}</p>
+          </div>
+        </div>
+        <div className='flex flex-col justify-center items-center max-w-[13rem] w-full gap-3'>
+          <div className='flex justify-center items-center'>
+            <h2 className='font-bold text-[Poppins] text-2xl sm:text-3xl md:text-3xl lg:text-4xl text-greenText'>{number2}</h2>
+          </div>
+          <div>
+            <p className='text-[Poppins] font-normal text-base sm:text-base md:text-base lg:text-xl xl:text-xl   opacity-50'>{text2}</p>
+          </div>
+        </div>
+        <div className='flex flex-col justify-center items-center max-w-[13rem] w-full gap-3'>
+          <div className='flex justify-center items-center'>
+            <h2 className='font-bold text-[Poppins] text-2xl sm:text-3xl md:text-3xl lg:text-4xl text-greenText'>{number3}</h2>
+          </div>
+          <div>
+            <p className='text-[Poppins] font-normal text-base sm:text-base md:text-base lg:text-xl xl:text-xl opacity-50'>{text3}</p>
+          </div>
+        </div>
+        <div className='flex flex-col justify-center items-center max-w-[13rem] w-full gap-3'>
+          <div className='flex justify-center items-center'>
+            <h2 className='font-bold text-[Poppins] text-2xl sm:text-3xl md:text-3xl lg:text-4xl text-greenText'>{number4}</h2>
+          </div>
+          <div>
+            <p className='text-[Poppins] font-normal text-base sm:text-base md:text-base lg:text-xl xl:text-xl  opacity-50'>{text4}</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/app/(pages)/partners/howitworks/page.tsx
+++ src/app/(pages)/partners/howitworks/page.tsx
@@ -11,7 +11,6 @@ function HowItsWorks() {
   return (
     <div className='w-screen flex flex-col justify-center items-center'>
       <div className='w-full flex justify-center items-center sm:mb-14 mb-5'>
-
         <FirstPartnerplatform
           headerBlackText1={"97% of partners say using"}
           headerGreenText1={"our platform"}
@@ -33,36 +32,31 @@ function HowItsWorks() {
 
       </div>
 
+      <div className='w-full flex justify-center items-center mb-14 mt-4'>
+        <HowItWorksSection
+          heading={'How It Works'}
 
-      <HowItWorksSection heading={''}
-        number1={'1'}
-        text1={'Begin the partnership process by visiting our website and accessing the partnership registration page. Fill out and submit the partnership registration form with your organizations details.'}
-        header1={'Register on our Website'}
-        image1={'/LaptopHowItWorks.png'}
+          number1={'1'}
+          header1={'Register on our Website'}
+          text1={'Begin the partnership process by visiting our website and accessing the partnership registration page. Fill out and submit the partnership registration form with your organizations details.'}
+          image1={'/LaptopHowItWorks.png'}
 
-        number2={'2'}
-        text2={'Our partnership team will carefully review your registration submission and evaluate your organization based on criteria such as qualifications, expertise, resources, and potential synergy with our platform.'}
-        header2={'Evaluation and Assessment'}
-        image2={'/TwoPersonTalkHowItWorks.png'}
+          number2={'2'}
+          header2={'Evaluation and Assessment'}
+          text2={'Our partnership team will carefully review your registration submission and evaluate your organization based on criteria such as qualifications, expertise, resources, and potential synergy with our platform.'}
+          image2={'/TwoPersonTalkHowItWorks.png'}
 
-        number3={'3'}
-        text3={'Our collaboration plan aims to leverage each partner strengths and resources to achieve our shared objectives'}
-        header3={'Collaboration Planning'}
-        image3={'/GroupTalkHowItWorks.png'}
+          number3={'3'}
+          header3={'Collaboration Planning'}
+          text3={'Our collaboration plan aims to leverage each partner strengths and resources to achieve our shared objectives'}
+          image3={'/GroupTalkHowItWorks.png'}
 
-        number4={'4'}
-        text4={'With the collaboration plan in place, we will initiate the implementation phase, working together to execute the agreed-upon activities.'}
-        header4={'Implementation and Execution'}
-        image4={'/DoctorLaptopHowItWorks.png'} />
+          number4={'4'}
+          header4={'Implementation and Execution'}
+          text4={'With the collaboration plan in place, we will initiate the implementation phase, working together to execute the agreed-upon activities.'}
+          image4={'/DoctorLaptopHowItWorks.png'} />
 
-
-
-
-
-
-
-
-
+      </div>
     </div>
   )
 }
