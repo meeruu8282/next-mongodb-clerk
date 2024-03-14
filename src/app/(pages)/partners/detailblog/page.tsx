527786fa6f5b36e4724402e9e8033db31e4cfbee
Anton Sjöstrand <asjoa99@hotmail.com>
2024-03-14 17:03:16 +0100

    made the page file and add almost all elements

---
+++ src/app/(pages)/partners/detailblog/page.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import Navbar from '../Components/navbar/Navbar'
+import NavbarMobile from '../Components/navbar/NavbarMobile'
+import Image from "next/image";
+
+const DetailBlog = () => {
+    return (
+        <>
+            <div>
+                <div className=" max-h-32px top-25px left-144px ">
+                    <div className="hidden md:block">
+                        <Navbar />
+                    </div>
+                    <div className="block md:hidden">
+                        <NavbarMobile />
+                    </div>
+                </div>
+            </div>
+            <div className='w-screen flex flex-col justify-center items-center'>
+
+                <div className='flex flex-col w-[90%] h-auto gap-6'>
+                    <div className='flex justify-start'>
+                        <h3 className='text-[Poppins] font-normal text-greenText text-sm sm:text-base md:text-base lg:text-base 2xl:text-lg'>Friday, 26 May 2024</h3>
+
+                    </div>
+                    <div className='flex justify-start'>
+                        <h1 className='text-[Poppins] font-norml text-2xl sm:text-3xl md:text-4xl lg:text-5xl '>Strengthening Cancer Care: A <br /> Collaborative Research Initiative</h1>
+
+                    </div>
+                    <div className='flex justify-end'>
+                        <p className='text-[Poppins] text-[#8E8E8F] font-normal text-sm sm:text-base md:text-base 2xl:text-lg'>2 Minutes read</p>
+
+                    </div>
+
+
+                </div>
+
+                <div className='flex justify-center w-[90%]'>
+                    <Image
+
+                        layout="responsive"
+                        width={1191}
+                        height={525}
+                        src="/DoctorsDetailBlog.png"
+                        alt="mobilephone"
+                    />
+
+
+                </div>
+                <div className='flex flex-col justify-center w-[85%] gap-5'>
+                    <p>Advancing cancer care requires continuous research, innovation, and collaboration. We embarked on a collaborative research initiative aimed at improving cancer detection and treatment outcomes.</p>
+                    <p>Our joint research team comprised leading oncologists, researchers, and data scientists, pooling their expertise to analyze vast datasets and develop predictive models for early cancer detection. By combining our organizations' resources and knowledge, we were able to access a diverse range of patient data, enabling us to identify patterns, risk factors, and potential treatment approaches.</p>
+                    <p>
+                        The outcomes of this collaboration have been remarkable. Through the application of artificial intelligence and machine learning algorithms, we have achieved higher accuracy rates in diagnosing certain types of cancers at earlier stages. This breakthrough has the potential to save lives and transform the field of oncology.
+                        High levels of bad cholesterol lead to fatty deposits building up in blood vessels, which can eventually cause heart attacks or strokes.</p>
+
+                </div>
+
+
+
+            </div>
+        </>
+    )
+}
+
+export default DetailBlog
