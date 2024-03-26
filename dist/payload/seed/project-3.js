99a91e171d7b238d7d72b5a9fe7ae72aa42d4564
jonatangroncode <jonatan.gron.code@hotmail.com>
2024-03-26 11:58:49 +0100

    adding paylod in tsconfigen

---
+++ dist/payload/seed/project-3.js
@@ -0,0 +1,56 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.project3 = void 0;
+exports.project3 = {
+    title: 'Project 3',
+    slug: 'project-3',
+    _status: 'published',
+    meta: {
+        title: 'Project 1',
+        description: 'This is the third project.',
+        image: '{{IMAGE}}',
+    },
+    hero: {
+        type: 'lowImpact',
+        links: null,
+        richText: [
+            {
+                children: [
+                    {
+                        text: 'Project 3',
+                    },
+                ],
+                type: 'h1',
+            },
+        ],
+        media: null,
+    },
+    layout: [
+        {
+            blockType: 'content',
+            columns: [
+                {
+                    size: 'twoThirds',
+                    richText: [
+                        {
+                            children: [
+                                {
+                                    text: "This content is completely dynamic using custom layout building blocks configured in the CMS. This can be anything you'd like from rich text and images, to highly designed, complex components.",
+                                },
+                            ],
+                        },
+                    ],
+                    link: {
+                        reference: {
+                            relationTo: 'pages',
+                            value: '',
+                        },
+                        url: '',
+                        label: '',
+                    },
+                },
+            ],
+        },
+    ],
+    relatedProjects: [], // this is populated by the seed script
+};
