0b0574b76bb4b71e36d177c54f82a21a205f8afb
EmilyGhazaleh <151723371+EmilyGhazaleh@users.noreply.github.com>
2024-03-27 10:18:55 +0100

    update Advantage

--- src/app/(pages)/partners/advantage/page.tsx
+++ src/app/(pages)/partners/advantage/page.tsx
@@ -1,9 +1,5 @@
-
 import HelpCard from "./advantageCard";
-
-import React from 'react'
-import Navbar from '../Components/navbar/Navbar'
-import NavbarMobile from '../Components/navbar/NavbarMobile'
+import PartnerAdvantage from "./partnerAdvantage";
 
 
 
@@ -38,13 +34,17 @@ export default function () {
         } 
         link={""}
         />
-      
+    <PartnerAdvantage
+    
+     image={"/partnerAdvantage.png"}
+     header={"Partner Advantage"}
+     paragraph={"Partnering with us opens up a world of advantages, incloding access to a vast network, collaborative opprtunities, enhanced credibility, increased exposure and marketing, technical support and resources, and the opportunity for continuous growth and collaboration. By becoming our partner, you can leverage these advantages to expand your reach, enhance your organization's reputation, and drive positive change in the healthcare industry."} 
+     buttonText={"Contact Us"}
+     subText={"We are excited to explore the possibilites together!"}>
+
+    </PartnerAdvantage>
      
     </div>
     </div>
   )
 };
-
-
-export default page
-
