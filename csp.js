d0968ae5756f688a57da917455a0d758e9527f41
jonatangroncode <jonatan.gron.code@hotmail.com>
2024-04-22 15:58:46 +0200

    fixing clerk csp policys

--- csp.js
+++ csp.js
@@ -1,19 +1,20 @@
 const policies = {
   'default-src': ["'self'"],
-  'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'https://maps.googleapis.com'],
+  'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'https://maps.googleapis.com', 'https://present-fawn-5.clerk.accounts.dev'],
+  'worker-src': ["'self'", 'blob:'],
   'child-src': ["'self'"],
   'style-src': ["'self'", "'unsafe-inline'", 'https://fonts.googleapis.com'],
-  'img-src': ["'self'", 'https://raw.githubusercontent.com'],
+  'img-src': ["'self'", 'https://raw.githubusercontent.com', 'https://img.clerk.com'],
   'font-src': ["'self'"],
-  'frame-src': ["'self'"],
-  'connect-src': ["'self'", 'https://maps.googleapis.com'],
-}
+  'frame-src': ["'self'", 'https://present-fawn-5.clerk.accounts.dev'],
+  'connect-src': ["'self'", 'https://maps.googleapis.com', 'https://present-fawn-5.clerk.accounts.dev']
+};
 
 module.exports = Object.entries(policies)
   .map(([key, value]) => {
     if (Array.isArray(value)) {
-      return `${key} ${value.join(' ')}`
+      return `${key} ${value.join(' ')}`;
     }
-    return ''
+    return '';
   })
-  .join('; ')
+  .join('; ');
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ const poppins = Poppins({
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <html lang="en" className={`${poppins.variable} ${inter.variable}`}>
         <body>
           <Providers>
