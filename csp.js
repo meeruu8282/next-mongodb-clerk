e05bb411c62ad3fb2e9c9f31f149d87b09bfd566
jonatangroncode <jonatan.gron.code@hotmail.com>
2024-04-22 11:13:16 +0200

    csp changes

--- csp.js
+++ csp.js
@@ -1,12 +1,12 @@
 const policies = {
   'default-src': ["'self'"],
-  'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'https://maps.googleapis.com'],
+  'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'https://maps.googleapis.com', 'https://your-clerk-domain.com'],
   'child-src': ["'self'"],
-  'style-src': ["'self'", "'unsafe-inline'", 'https://fonts.googleapis.com'],
-  'img-src': ["'self'", 'https://raw.githubusercontent.com'],
+  'style-src': ["'self'", "'unsafe-inline'", 'https://fonts.googleapis.com', 'https://your-clerk-domain.com'],
+  'img-src': ["'self'", 'https://raw.githubusercontent.com', 'https://your-clerk-domain.com'],
   'font-src': ["'self'"],
   'frame-src': ["'self'"],
-  'connect-src': ["'self'", 'https://maps.googleapis.com'],
+  'connect-src': ["'self'", 'https://maps.googleapis.com', 'https://your-clerk-domain.com'],
 }
 
 module.exports = Object.entries(policies)
