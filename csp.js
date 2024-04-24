80877a3ee7e12fd08acb707781db09200b1bccb5
jonatangroncode <jonatan.gron.code@hotmail.com>
2024-04-24 12:41:50 +0200

    security in csp

--- csp.js
+++ csp.js
@@ -1,13 +1,13 @@
 const policies = {
   'default-src': ["'self'"],
-  'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'https://maps.googleapis.com', 'https://present-fawn-5.clerk.accounts.dev', 'https://vercel.live'],
+  'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'https://maps.googleapis.com', 'https://present-fawn-5.clerk.accounts.dev', 'https://vercel.live', 'https://vercel.com'],
   'worker-src': ["'self'", 'blob:'],
   'child-src': ["'self'", 'https://vercel.live'],
-  'style-src': ["'self'", "'unsafe-inline'", 'https://fonts.googleapis.com'],
-  'img-src': ["'self'", 'https://raw.githubusercontent.com', 'https://img.clerk.com'],
+  'style-src': ["'self'", "'unsafe-inline'", 'https://fonts.googleapis.com', 'https://vercel.live', 'https://vercel.com'],
+  'img-src': ["'self'", 'https://raw.githubusercontent.com', 'https://img.clerk.com', 'https://vercel.com'],
   'font-src': ["'self'"],
   'frame-src': ["'self'", 'https://present-fawn-5.clerk.accounts.dev', 'https://vercel.live'],
-  'connect-src': ["'self'", 'https://maps.googleapis.com', 'https://present-fawn-5.clerk.accounts.dev', 'https://api.clerk.com', 'https://vercel.live'],
+  'connect-src': ["'self'", 'https://maps.googleapis.com', 'https://present-fawn-5.clerk.accounts.dev', 'https://api.clerk.com', 'https://vercel.live', 'https://sockjs-us3.pusher.com', 'https://vercel.com'],
 };
 
 module.exports = Object.entries(policies)
