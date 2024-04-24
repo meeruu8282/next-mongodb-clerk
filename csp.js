d197166812bf5bb5ecfc6eeba4901a0f3f79579c
jonatangroncode <jonatan.gron.code@hotmail.com>
2024-04-24 12:35:33 +0200

    added vercel in csp connect

--- csp.js
+++ csp.js
@@ -2,12 +2,12 @@ const policies = {
   'default-src': ["'self'"],
   'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'https://maps.googleapis.com', 'https://present-fawn-5.clerk.accounts.dev', 'https://vercel.live'],
   'worker-src': ["'self'", 'blob:'],
-  'child-src': ["'self'"],
+  'child-src': ["'self'", 'https://vercel.live'],
   'style-src': ["'self'", "'unsafe-inline'", 'https://fonts.googleapis.com'],
   'img-src': ["'self'", 'https://raw.githubusercontent.com', 'https://img.clerk.com'],
   'font-src': ["'self'"],
-  'frame-src': ["'self'", 'https://present-fawn-5.clerk.accounts.dev'],
-  'connect-src': ["'self'", 'https://maps.googleapis.com', 'https://present-fawn-5.clerk.accounts.dev', 'https://api.clerk.com']
+  'frame-src': ["'self'", 'https://present-fawn-5.clerk.accounts.dev', 'https://vercel.live'],
+  'connect-src': ["'self'", 'https://maps.googleapis.com', 'https://present-fawn-5.clerk.accounts.dev', 'https://api.clerk.com', 'https://vercel.live'],
 };
 
 module.exports = Object.entries(policies)
@@ -18,4 +18,3 @@ module.exports = Object.entries(policies)
     return '';
   })
   .join(';');
-
