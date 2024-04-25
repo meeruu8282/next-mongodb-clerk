6d87e79af57c91b1a1569bb575285c7115188980
jonatangroncode <jonatan.gron.code@hotmail.com>
2024-04-25 11:32:53 +0200

    remove frame

--- csp.js
+++ csp.js
@@ -9,7 +9,10 @@ const policies = {
   'connect-src': ["'self'", 'https://maps.googleapis.com', 'https://present-fawn-5.clerk.accounts.dev', 'https://api.clerk.com', 'https://vercel.live', 'https://sockjs-us3.pusher.com', 'https://vercel.com'],
 };
 
-module.exports = Object.entries(policies)
+// Filter out 'frame-src' if it's present
+delete policies['frame-src'];
+
+export default Object.entries(policies)
   .map(([key, value]) => {
     if (Array.isArray(value)) {
       return `${key} ${value.join(' ')}`;
