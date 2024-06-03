02cc226529163e678a9921386144fbb241729313
meeru mohanakumar <meeru284@gmail.com>
2024-06-03 10:00:33 +0200

    first commit

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -55,34 +55,34 @@ export async function POST(req: Request) {
   const { id } = evt.data;
   const eventType = evt.type;
 
-  // CREATE User in mongodb
-  if (eventType === "user.created") {
-    const { id, email_addresses, image_url, first_name, last_name, username } =
-      evt.data;
+//   // CREATE User in mongodb
+//   if (eventType === "user.created") {
+//     const { id, email_addresses, image_url, first_name, last_name, username } =
+//       evt.data;
 
-    const user = {
-      clerkId: id,
-      email: email_addresses[0].email_address,
-      username: username!,
-      firstName: first_name,
-      lastName: last_name,
-      photo: image_url,
-    };
+//     const user = {
+//       clerkId: id,
+//       email: email_addresses[0].email_address,
+//       username: username!,
+//       firstName: first_name,
+//       lastName: last_name,
+//       photo: image_url,
+//     };
 
-    console.log(user);
+//     console.log(user);
 
-    const newUser = await createUser(user);
+//     const newUser = await createUser(user);
 
-    if (newUser) {
-      await clerkClient.users.updateUserMetadata(id, {
-        publicMetadata: {
-          userId: newUser._id,
-        },
-      });
-    }
+//     if (newUser) {
+//       await clerkClient.users.updateUserMetadata(id, {
+//         publicMetadata: {
+//           userId: newUser._id,
+//         },
+//       });
+//     }
 
-    return NextResponse.json({ message: "New user created", user: newUser });
-  }
+//     return NextResponse.json({ message: "New user created", user: newUser });
+//   }
 
   console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
   console.log("Webhook body:", body);
