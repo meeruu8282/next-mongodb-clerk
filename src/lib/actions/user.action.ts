728056eca8d422245ee2a5a7a80499a34dea6f14
meeru mohanakumar <meeru284@gmail.com>
2024-06-10 18:46:45 +0200

    env

--- src/lib/actions/user.action.ts
+++ src/lib/actions/user.action.ts
@@ -4,11 +4,21 @@ import User from "@/lib/modals/user.modal";
 import { connect } from "@/lib/db";
 
 export async function createUser(user: any) {
+  console.log("Starting user creation process...");
+  console.log("User data received:", user);
+
   try {
+    console.log("Connecting to MongoDB...");
     await connect();
+    console.log("Successfully connected to MongoDB.");
+
+    console.log("Creating a new user in the database...");
     const newUser = await User.create(user);
+    console.log("User successfully created:", newUser);
+
     return JSON.parse(JSON.stringify(newUser));
   } catch (error) {
-    console.log(error);
+    console.error("Error occurred during user creation:", error);
+    throw new Error("Error creating user");
   }
 }
